b6ef97157229aca6d2c209474b964b75f770edb1
Hari8036 <chavanharidas777@gmail.com>
2022-10-04 18:38:30 +0530

    First Commit

--- src/main/java/com/irissoftware/spring/actuator/api/SpringBootActuatorApplication.java
+++ src/main/java/com/irissoftware/spring/actuator/api/SpringBootActuatorApplication.java
@@ -1,4 +1,4 @@
-package com.javatechie.spring.actuator.api;
+package com.irissoftware.spring.actuator.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
